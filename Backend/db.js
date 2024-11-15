bb4f1e629df0b4f57643f2f0da0b74f2d89c2f01
ABCCheng <47651956+ABCCheng@users.noreply.github.com>
2024-11-15 10:59:41 -0500

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,1 +1,15 @@
 
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: '',
+  port: '',
+  user: '',
+  password: '',
+  database: '',
+  waitForConnections: true,
+  connectionLimit: 10, 
+  queueLimit: 0,      
+});
+
+module.exports = pool;
